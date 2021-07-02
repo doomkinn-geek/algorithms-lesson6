030f4a64ef4d748a57fba2f6a8ff16df53d48e53
doomkinn@yandex.ru <doomkinn@yandex.ru>
2021-07-02 16:16:17 +0300

    half BTS

--- algorithms-lesson6/Graph.cs
+++ algorithms-lesson6/Graph.cs
@@ -9,12 +9,33 @@ namespace algorithms_lesson6
     {
         public int Value { get; set; }
         public List<Edge> Edges { get; set; } //исходящие связи
+
+        public Node()
+        {
+            Edges = new List<Edge>();
+        }
+        public Node(int data)
+        {
+            Edges = new List<Edge>();
+            Value = data;
+        }
     }
 
     public class Edge //Ребро
     {
         public int Weight { get; set; } //вес связи
         public Node Node { get; set; } // узел, на который связь ссылается
+
+        public Edge(Node node, int weight)
+        {
+            Node = node;
+            Weight = weight;
+        }
+
+        public Edge(Node node)
+        {
+            Node = node;            
+        }
     }
     class Graph
     {
@@ -23,14 +44,83 @@ namespace algorithms_lesson6
 
         public Node AddNode(int data)
         {
-            if (ContainsNode(data))
+            if (ContainNode(data))
                 throw new InvalidOperationException("Вершина с таким значением уже существует");
 
-            var node = new Node();
+            var node = new Node(data);
             Nodes.Add(node);
             return node;
         }
 
-        public bool ContainsNode(int data) => Nodes.Any(x => x.Value.Equals(data));
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="graphNode">graphNode - узел, НА который ссылается связь</param>
+        /// <returns></returns>
+        public Edge AddEdge(Node graphNode)
+        {
+            var edge = new Edge(graphNode);
+            Edges.Add(edge);
+            return edge;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="graphNode">graphNode - узел, НА который ссылается связь</param>
+        /// <param name="weight"></param>
+        /// <returns></returns>
+        public Edge AddEdge(Node graphNode, int weight)
+        {
+            if (!ContainNode(graphNode.Value))
+                throw new InvalidOperationException("Вершина не принадлежит графу");
+
+            var edge = new Edge(graphNode, weight);
+            Edges.Add(edge);
+            return edge;
+        }
+
+        public bool ContainNode(int data) => Nodes.Any(x => x.Value.Equals(data));
+
+        public void BFS(Node root)
+        {
+            if (root == null)
+                return;
+
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                Node node = queue.Dequeue();
+                if (node != null)
+                {
+                    Console.WriteLine(node.Value);
+                    foreach (Edge e in node.Edges)
+                    {
+                        if (e != null)
+                            queue.Enqueue(e.Node);
+                    }
+                }
+            }
+        }
+
+        public void DFS(Node root)
+        {
+            /*if (root == null)
+                return;
+
+            Stack<Node> stack = new Stack<Node>();
+            stack.Push(root);
+            while (stack.Count > 0)
+            {
+                Node node = stack.Pop();
+                Console.WriteLine(node.Data);
+
+                if (node.Right != null)
+                    stack.Push(node.Right);
+                if (node.Left != null)
+                    stack.Push(node.Left);
+            }*/
+        }
     }
 }
--- algorithms-lesson6/Program.cs
+++ algorithms-lesson6/Program.cs
@@ -6,7 +6,36 @@ namespace algorithms_lesson6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Graph graph = new Graph();
+            var n1 = graph.AddNode(0);
+            var n2 = graph.AddNode(1);
+            var n3 = graph.AddNode(2);
+            var n4 = graph.AddNode(3);
+            var n5 = graph.AddNode(4);
+            var n6 = graph.AddNode(5);
+            var n7 = graph.AddNode(6);
+            var n8 = graph.AddNode(7);
+            var n9 = graph.AddNode(8);
+
+            var e1 = graph.AddEdge(n2);
+            var e2 = graph.AddEdge(n3);
+            var e3 = graph.AddEdge(n4);
+            var e4 = graph.AddEdge(null);
+            var e5 = graph.AddEdge(n7);
+            var e6 = graph.AddEdge(null);
+            var e7 = graph.AddEdge(null);
+            var e8 = graph.AddEdge(null);
+
+            n1.Edges.Add(e1);
+            n1.Edges.Add(e2);
+            n1.Edges.Add(e3);
+            n2.Edges.Add(e4);
+            n2.Edges.Add(e5);
+            n6.Edges.Add(e6);
+            n4.Edges.Add(e7);
+            n4.Edges.Add(e8);
+
+            graph.BFS(n1);
         }
     }
 }
